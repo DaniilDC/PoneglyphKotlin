c38fc23fb0d7be6b2dcc236e30663dd2bd028dc5
Daniil Chernyshov <57674031+DaniilDC@users.noreply.github.com>
2024-05-04 15:40:36 +0300

    Initial commit. This is not a first time of saving the project. Project was saved to a local repository many times, so now I decided to create a GitHub repository.

---
+++ app/src/main/java/com/technifysoft/poneglyph/AdapterCategory.kt
@@ -0,0 +1,90 @@
+package com.technifysoft.poneglyph
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.FirebaseDatabase
+import com.technifysoft.poneglyph.databinding.RowCategoryBinding
+
+class AdapterCategory: RecyclerView.Adapter<AdapterCategory.HolderCategory>, Filterable{
+
+    private val context: Context
+    public var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var binding:RowCategoryBinding
+    private var filterList: ArrayList<ModelCategory>
+    private var filter: FilterCategory? = null
+
+    constructor(context: Context, categoryArrayList: ArrayList<ModelCategory>) {
+        this.context = context
+        this.categoryArrayList = categoryArrayList
+        this.filterList = categoryArrayList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderCategory {
+        binding = RowCategoryBinding.inflate(LayoutInflater.from(context), parent, false)
+        return HolderCategory(binding.root)
+    }
+
+
+    override fun onBindViewHolder(holder: HolderCategory, position: Int) {
+        val model = categoryArrayList[position]
+        val id = model.id
+        val category = model.category
+        val uid = model.uid
+        val timestamp = model.timestamp
+
+        holder.categoryTv.text = category
+
+        holder.deleteBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Delete")
+                .setMessage("Are you sure deleting this category?")
+                .setPositiveButton("Confirm") {a, d->
+                    Toast.makeText(context, "Deleting..", Toast.LENGTH_SHORT).show()
+                    deleteCategory(model, holder)
+                }
+                .setNegativeButton("Cancel") {a, d->
+                    a.dismiss()
+                }
+                .show()
+        }
+    }
+
+    private fun deleteCategory(model: ModelCategory, holder: HolderCategory) {
+        val id = model.id
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.child(id)
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e->
+                Toast.makeText(context, "Unable delete due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    override fun getItemCount(): Int {
+        return categoryArrayList.size
+    }
+
+    override fun getFilter(): Filter {
+        if (filter == null) {
+            filter = FilterCategory(filterList, this)
+        }
+        return filter as FilterCategory
+    }
+
+    inner class HolderCategory(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var categoryTv:TextView = binding.categoryTv
+        var deleteBtn:ImageButton = binding.deleteBtn
+    }
+
+}
